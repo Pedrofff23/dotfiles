3a3d718cfedb4a6f12c349934ba56fe3675270c5
pedrofff <pmendes09@hotmail.com>
2025-06-01 22:05:40 -0300

    💄 Changing dashboard plugin && add animation

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>h", Snacks.dashboard.open, { desc = "Open mini starter" })
+-- vim.keymap.set("n", "<leader>h", Snacks.dashboard.open, { desc = "Open mini starter" })
+
+vim.keymap.set("n", "<leader>h", function()
+  require("alpha").start()
+end, { desc = "Open Alpha dashboard" })
 
 vim.keymap.set("n", "<M-w>", "cgn", { noremap = true, desc = "Change next search match" })
